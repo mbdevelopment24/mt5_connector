30d6ef793790eba8db0366c1bc3f2451108bcd7f
mbdevelopment24 <159246246+mbdevelopment24@users.noreply.github.com>
2024-11-30 14:52:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,32 @@
 """
 Telegram bot configuration
 """
-TELEGRAM_TOKEN = '6405881837:AAFtZy4KfSwfqlCcA2q4nF8K30FXfeUMOX4'
-CHANNEL_USERNAME = '-1002082809410'
+TELEGRAM_TOKEN = 'Token'
+CHANNEL_USERNAME = 'Channel-ID'
 
 """
 MetaTrader 5 configuration
 """
-MT5_LOGIN = 1510023458
-MT5_SERVER = 'FTMO-Demo'
-MT5_PASSWORD = '?=?rd@X6F=4'
-MT5_PATH = 'C:\\Program Files\\MetaTrader 5\\terminal64.exe'
+MT5_LOGIN = MT5Logon
+MT5_SERVER = 'Server'
+MT5_PASSWORD = 'Pass'
+MT5_PATH = 'MT5_Path'
+
+"""
+RISK
+"""
+TRADE_RISK = 50
 
 """
 Order configuration
 Change to 'LIMIT' for limit orders
 """
 ORDER_TYPE = 'MARKET'
+
+"""
+Take Profit / Stop Loss management
+"""
+TP1_PERCENT_TAKE = 50  # Take 50% at TP1
+TP1_TOLERANCE_CENTS = 5  # Tolerance before TP1, in cents
+
 
